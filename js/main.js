b3f95d67b270dd083bd63acf776a2f9674ce2f9d
ibraheemmobrad <ibraheem.mobrad@gmail.com>
2021-08-28 04:42:09 +0300

    update project

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-function add_seen(id, name, tab_name) {
-  var seen_container = document.createElement("div");
-  seen_container.appendChild(document.getElementById(tab_name));
-  var seen_head = document.createElement("h2");
-  seen_head.createAttribute("class");
-  seen_container.ATTRIBUTE_NODE("phase-head");
-  seen_head.TEXT_NODE(name);
-  seen_head.appendChild(seen_container);
-}
-add_seen(1, "المرحلة الإبتدائية", "tab1-1");
-add_seen(2, "المرحلة المتوسطة", "tab1-1");
+document.write ("<h1>hi</h1>").style
+(color="blue".
+font-size:"80px".
+font-weight="bold".
+text-align="center".
+font-fammily="Arial";
+);
