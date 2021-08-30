2055366ee4ca469c12541a88987c7ffa014a5716
ibraheemmobrad <ibraheem.mobrad@gmail.com>
2021-08-30 13:04:24 +0300

    update project

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 function add_book (book_id,book_name,book_path){
     let book = `<div id="book${book_id}">
     <li>
-    <a href="${book_path}">${book_name}</a>
+    <a href="${book_path}">${book_name}<a/>
     </li>
-</div>}`;
+    </div>`;
     document.write(book)
 }
