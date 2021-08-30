70e0ec7f6424220cb52535372a88be27c01843b7
ibraheemmobrad <ibraheem.mobrad@gmail.com>
2021-08-30 12:49:38 +0300

    create nu system to add books

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//add book funktion
+function add_book (book_id,book_name,book_path){
+    let book = `<div id="book${book_id}">
+    <li>
+    <a href="${book_path}">${book_name}</a>
+    </li>
+</div>}`;
+    document.write(book)
+}
