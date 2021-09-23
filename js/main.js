fef8d996877f46575607a1bb5c66a29200f3332e
ibraheemmobrad <ibraheem.mobrad@gmail.com>
2021-09-23 05:56:24 +0300

    update project

--- js/main.js
+++ js/main.js
@@ -354,6 +354,11 @@ addClassContent(
   "class17",
   "https://moe.edu.kw/student/curriculum/%D9%83%D8%AA%D8%A8%20%D8%A7%D9%84%D8%AA%D8%B9%D9%84%D9%8A%D9%85%20%D8%A7%D9%84%D8%B9%D8%A7%D9%85%202021%20-%202022/2%20%D8%A7%D9%84%D9%85%D8%B1%D8%AD%D9%84%D8%A9%20%D8%A7%D9%84%D8%A5%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%8A%D8%A9/5/%D8%A7%D9%84%D9%81%D8%B5%D9%84%20%D8%A7%D9%84%D8%AF%D8%B1%D8%A7%D8%B3%D9%8A%20%D8%A7%D9%84%D8%A3%D9%88%D9%84/%D9%83%D8%B1%D8%A7%D8%B3%D8%A9%20%D8%A7%D9%84%D8%AE%D8%B7%20%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A%20%D8%B5%205.pdf"
 );
+addClassContent(
+  "اللغة العربية - الفصل الدراسي الأول",
+  "class17",
+  "https://www.moe.edu.kw/student/curriculum/%D9%83%D8%AA%D8%A8%20%D8%A7%D9%84%D8%AA%D8%B9%D9%84%D9%8A%D9%85%20%D8%A7%D9%84%D8%B9%D8%A7%D9%85%202021%20-%202022/2%20%D8%A7%D9%84%D9%85%D8%B1%D8%AD%D9%84%D8%A9%20%D8%A7%D9%84%D8%A5%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%8A%D8%A9/5/%D8%A7%D9%84%D9%81%D8%B5%D9%84%20%D8%A7%D9%84%D8%AF%D8%B1%D8%A7%D8%B3%D9%8A%20%D8%A7%D9%84%D8%A3%D9%88%D9%84/%D9%84%D8%BA%D8%AA%D9%8A%20%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9%20%D8%B5%205%20%D8%AC%201.pdf"
+);
 addPhase("phase5", "المرحلة المتوسطة", "tab3");
 addClass("class18", "الصف السادس", "phase5");
 addClass("class19", "الصف السابع", "phase5");
