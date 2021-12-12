a9725742075fcaaba66f7161115cf4a037d62192
ibraheemmobrad <ibraheem.mobrad@gmail.com>
2021-12-12 20:29:07 +0300

    update project

--- data/phases.js
+++ data/phases.js
@@ -3,4 +3,4 @@ addPhase("phase2", "المرحلة المتوسطة", "tab2");
 addPhase("phase3", "المرحلة الثانوية", "tab2");
 addPhase("phase4", "المرحلة الإبتدائية", "tab3");
 addPhase("phase5", "المرحلة المتوسطة", "tab3");
-addPhase("phase6", "المرحلة الثانوية", "tab3");
+// addPhase("phase6", "المرحلة الثانوية", "tab3");
